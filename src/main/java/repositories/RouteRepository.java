614f1afecdf3a04bb932e876ba853547fed5a99e
Jose <me@josearodriguez.me>
2017-04-20 20:15:51 +0200

    Paginación en busqueda de rutas. Se ha paginado las rutas encontradas tras realizar la búsqueda.

--- src/main/java/repositories/RouteRepository.java
+++ src/main/java/repositories/RouteRepository.java
@@ -1,6 +1,5 @@
 package repositories;
 
-import java.util.Collection;
 import java.util.Date;
 
 import org.springframework.data.domain.Page;
@@ -17,7 +16,7 @@ public interface RouteRepository extends JpaRepository<Route, Integer> {
 	@Query("select r from Route r where ?1 like CONCAT('%',r.origin,'%') AND ?2 like CONCAT('%',r.destination,'%') AND (?3 IS NULL OR (year(r.departureTime) = year(?3) AND month(r.departureTime) = month(?3) AND day(r.departureTime) = day(?3)))"
 			+ "AND (?4 IS NULL OR r.departureTime >= ?4) AND (?5 IS NULL OR r.itemEnvelope = ?5 OR r.itemEnvelope LIKE 'both')"
 			+ "AND (?6 IS NULL OR (select sp.size from SizePrice sp where sp.route.id = r.id AND sp.size = ?6) IS NOT NULL)")
-	Collection<Route> searchRoute(String origin, String destination, Date date, Date time, String envelope, String itemSize);
+	Page<Route> searchRoute(String origin, String destination, Date date, Date time, String envelope, String itemSize,Pageable page);
 	
 	@Query("select r from Route r where r.creator.id = ?1")
 	Page<Route> findAllByUserId(int userId, Pageable page);
