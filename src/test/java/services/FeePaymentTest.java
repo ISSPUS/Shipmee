aedc7a228f2f7f59314397cfb8e7f346df0082de
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-04-23 01:55:53 +0200

    Payment tests - Route & RouteOffer uncompleted due to possible code bug

--- src/test/java/services/FeePaymentTest.java
+++ src/test/java/services/FeePaymentTest.java
@@ -19,8 +19,13 @@ import org.springframework.util.Assert;
 
 import domain.CreditCard;
 import domain.FeePayment;
+import domain.Route;
+import domain.RouteOffer;
 import domain.Shipment;
 import domain.ShipmentOffer;
+import domain.SizePrice;
+import domain.User;
+import domain.Vehicle;
 import domain.form.FeePaymentForm;
 import services.form.FeePaymentFormService;
 import utilities.AbstractTest;
@@ -46,6 +51,21 @@ public class FeePaymentTest extends AbstractTest {
 	
 	@Autowired
 	private ShipmentOfferService shipmentOfferService;
+	
+	@Autowired
+	private RouteService routeService;
+	
+	@Autowired
+	private RouteOfferService routeOfferService;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private VehicleService vehicleService;
+	
+	@Autowired
+	private SizePriceService sizePriceService;
 	
 	// Test cases -------------------------------------------------------------
 
@@ -499,6 +519,212 @@ public class FeePaymentTest extends AbstractTest {
 		payment = feePaymentFormService.reconstruct(feePaymentForm);
 		payment = feePaymentService.save(payment);
 		
+		numberOfPaymentsAfter = feePaymentService.findAll().size();
+		numberOfPaymentPendingAfter = (int) feePaymentService.findAllPendingByUser(page).getTotalElements();
+		
+		unauthenticate();
+				
+		Assert.isTrue(numberOfPaymentsAfter - numberOfPaymentsBefore == 1, "Number of Payment must increase");
+		Assert.isTrue(numberOfPaymentPendingAfter - numberOfPaymentPendingBefore == 1, "Number of Pending Payments must increase");
+
+		authenticate("user1");
+		
+		payment = feePaymentService.findOne(payment.getId());
+		payment.setType("Accepted");
+		payment = feePaymentService.save(payment);
+		
+		numberOfPaymentAcceptedAfter = (int) feePaymentService.findAllAccepted(page).getTotalElements();
+		
+		unauthenticate();
+		
+		Assert.isTrue(numberOfPaymentAcceptedAfter - numberOfPaymentAcceptedBefore == 1, "Number of Accepted Payments must increase");
+		
+	}
+	
+	@Test
+	public void positiveCreatePaymentRouteOffer1(){
+		
+		Pageable page = new PageRequest(0, 10);
+		
+		Route route;
+		
+		Integer numberOfPaymentsBefore;
+		Integer numberOfPaymentsAfter;
+		Integer numberOfPaymentPendingBefore;
+		Integer numberOfPaymentPendingAfter;
+		Integer numberOfPaymentAcceptedBefore;
+		Integer numberOfPaymentAcceptedAfter;
+		
+		numberOfPaymentsBefore = feePaymentService.findAll().size();
+		
+		authenticate("user1");
+		
+		numberOfPaymentPendingBefore = (int) feePaymentService.findAllPendingByUser(page).getTotalElements();
+		numberOfPaymentAcceptedBefore = (int) feePaymentService.findAllAccepted(page).getTotalElements();
+		
+		Vehicle vehicle;
+		SizePrice sizePrice;
+		
+		route = routeService.create();
+		route.setOrigin("Sevilla");
+		route.setDestination("Luego");
+		Date departureTime = new GregorianCalendar(2017, Calendar.JULY, 01).getTime();
+		route.setDepartureTime(departureTime);
+		Date arrivalTime = new GregorianCalendar(2017, Calendar.JULY, 02).getTime();
+		route.setArriveTime(arrivalTime);
+		route.setItemEnvelope("Open");
+		vehicle = vehicleService.findAllNotDeletedByUser().iterator().next();
+		route.setVehicle(vehicle);
+		
+		sizePrice = sizePriceService.create();
+		sizePrice.setPrice(10.0);
+		sizePrice.setSize("L");
+		
+		route = routeService.save(route);
+		
+		sizePrice.setRoute(route);
+		sizePrice = sizePriceService.save(sizePrice);
+		
+		unauthenticate();
+		
+		RouteOffer routeOffer;
+		FeePaymentForm feePaymentForm;
+		FeePayment payment;
+		CreditCard creditCard;
+		
+		authenticate("user2");
+		
+		route = routeService.findOne(route.getId());
+		routeOffer = routeOfferService.create(route.getId());
+		routeOffer.setAmount(5.0);
+		routeOffer.setDescription("Mensaje de Prueba");
+		routeOffer = routeOfferService.save(routeOffer);
+		
+		feePaymentForm = feePaymentFormService.create(2, routeOffer.getId(), 0, 5.0, "Prueba de pago");
+		
+		/**
+		 * EN EL MÉTODO CREATE, NO SE PONE EL OFFERID EN EL CASO DE LAS ROUTEOFFER.
+		 */
+		
+		creditCard = new CreditCard();
+		creditCard.setHolderName("Nombre de Prueba");
+		creditCard.setBrandName("VISA");
+		creditCard.setNumber("4929772835813522");
+		creditCard.setExpirationMonth(6);
+		creditCard.setExpirationYear(2020);
+		creditCard.setCvvCode(123);
+		feePaymentForm.setCreditCard(creditCard);
+		
+		payment = feePaymentFormService.reconstruct(feePaymentForm);
+		payment = feePaymentService.save(payment);
+
+		unauthenticate();
+		
+		authenticate("user1");
+		
+		routeOfferService.accept(feePaymentForm.getOfferId());
+		
+		numberOfPaymentsAfter = feePaymentService.findAll().size();
+		numberOfPaymentPendingAfter = (int) feePaymentService.findAllPendingByUser(page).getTotalElements();
+		
+		unauthenticate();
+				
+		Assert.isTrue(numberOfPaymentsAfter - numberOfPaymentsBefore == 1, "Number of Payment must increase");
+		Assert.isTrue(numberOfPaymentPendingAfter - numberOfPaymentPendingBefore == 1, "Number of Pending Payments must increase");
+
+		authenticate("user1");
+		
+		payment = feePaymentService.findOne(payment.getId());
+		payment.setType("Accepted");
+		payment = feePaymentService.save(payment);
+		
+		numberOfPaymentAcceptedAfter = (int) feePaymentService.findAllAccepted(page).getTotalElements();
+		
+		unauthenticate();
+		
+		Assert.isTrue(numberOfPaymentAcceptedAfter - numberOfPaymentAcceptedBefore == 1, "Number of Accepted Payments must increase");
+		
+	}
+	
+	@Test
+	public void positiveCreatePaymentRoute1(){
+		
+		Pageable page = new PageRequest(0, 10);
+		
+		Route route;
+		
+		Integer numberOfPaymentsBefore;
+		Integer numberOfPaymentsAfter;
+		Integer numberOfPaymentPendingBefore;
+		Integer numberOfPaymentPendingAfter;
+		Integer numberOfPaymentAcceptedBefore;
+		Integer numberOfPaymentAcceptedAfter;
+		
+		numberOfPaymentsBefore = feePaymentService.findAll().size();
+		
+		authenticate("user1");
+		
+		numberOfPaymentPendingBefore = (int) feePaymentService.findAllPendingByUser(page).getTotalElements();
+		numberOfPaymentAcceptedBefore = (int) feePaymentService.findAllAccepted(page).getTotalElements();
+		
+		Vehicle vehicle;
+		SizePrice sizePrice;
+		
+		route = routeService.create();
+		route.setOrigin("Sevilla");
+		route.setDestination("Luego");
+		Date departureTime = new GregorianCalendar(2017, Calendar.JULY, 01).getTime();
+		route.setDepartureTime(departureTime);
+		Date arrivalTime = new GregorianCalendar(2017, Calendar.JULY, 02).getTime();
+		route.setArriveTime(arrivalTime);
+		route.setItemEnvelope("Open");
+		vehicle = vehicleService.findAllNotDeletedByUser().iterator().next();
+		route.setVehicle(vehicle);
+		
+		sizePrice = sizePriceService.create();
+		sizePrice.setPrice(10.0);
+		sizePrice.setSize("L");
+
+		route = routeService.save(route);
+		
+		sizePrice.setRoute(route);
+		sizePrice = sizePriceService.save(sizePrice);
+				
+		unauthenticate();
+		
+		RouteOffer routeOffer;
+		FeePaymentForm feePaymentForm;
+		FeePayment payment;
+		CreditCard creditCard;
+		
+		authenticate("user2");
+				
+		route = routeService.findOne(route.getId());
+		sizePrice = sizePriceService.findAllByRouteId(route.getId()).iterator().next();
+		routeOffer = routeService.contractRoute(route.getId(), sizePrice.getId());
+		
+		feePaymentForm = feePaymentFormService.create(1, routeOffer.getId(), sizePrice.getId(), 0.0, "");
+		
+		/**
+		 * EN EL MÉTODO CREATE, NO SE PONE EL OFFERID EN EL CASO DE LAS ROUTEOFFER.
+		 */
+				
+		creditCard = new CreditCard();
+		creditCard.setHolderName("Nombre de Prueba");
+		creditCard.setBrandName("VISA");
+		creditCard.setNumber("4929772835813522");
+		creditCard.setExpirationMonth(6);
+		creditCard.setExpirationYear(2020);
+		creditCard.setCvvCode(123);
+		feePaymentForm.setCreditCard(creditCard);
+		
+		payment = feePaymentFormService.reconstruct(feePaymentForm);
+		payment = feePaymentService.save(payment);
+
+		unauthenticate();
+		
+		authenticate("user1");
+				
 		numberOfPaymentsAfter = feePaymentService.findAll().size();
 		numberOfPaymentPendingAfter = (int) feePaymentService.findAllPendingByUser(page).getTotalElements();
 		
