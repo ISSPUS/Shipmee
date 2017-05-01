15caa7c1d3e7754a936185ab33d362dc5de3ecba
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-05-01 18:22:06 +0200

    [Search Engine] Not even a fix

--- src/main/java/repositories/ShipmentRepository.java
+++ src/main/java/repositories/ShipmentRepository.java
@@ -13,7 +13,8 @@ import domain.Shipment;
 @Repository
 public interface ShipmentRepository extends JpaRepository<Shipment, Integer> {
 	
-	@Query("select s from Shipment s where ?1 like CONCAT('%',s.origin,'%') AND ?2 like CONCAT('%',s.destination,'%') AND (?3 IS NULL OR (year(s.departureTime) = year(?3) AND month(s.departureTime) = month(?3) AND day(s.departureTime) = day(?3)))"
+	@Query("select s from Shipment s where ?1 like CONCAT('%',s.origin,'%') AND ?2 like CONCAT('%',s.destination,'%')"
+			+ "AND (?3 IS NULL OR (year(s.departureTime) = year(?3) AND month(s.departureTime) = month(?3) AND day(s.departureTime) = day(?3)))"
 			+ "AND (?4 IS NULL OR s.departureTime >= ?4) AND (?5 IS NULL OR s.itemEnvelope = ?5  OR s.itemEnvelope LIKE 'both') "
 			+ "AND (?6 IS NULL OR s.itemSize = ?6)")
 	Page<Shipment> searchShipment(String origin, String destination, Date date, Date time, String envelope, String itemSize,Pageable page);
