986a9d963e7627d6e968a20632ceb97406fa09e5
Manuel López Ruiz <manuellr.git@gmail.com>
2017-07-05 20:46:52 +0200

    Fixed: Ya se puede aceptar la oferta a una ruta pagada mediante el refresco de paypal

--- src/main/java/controllers/user/PayPalUserController.java
+++ src/main/java/controllers/user/PayPalUserController.java
@@ -26,6 +26,7 @@ import domain.PayPalObject;
 import domain.ShipmentOffer;
 import services.FeePaymentService;
 import services.PayPalService;
+import services.RouteOfferService;
 import services.ShipmentOfferService;
 import utilities.PayPalConfig;
 
@@ -43,6 +44,9 @@ public class PayPalUserController extends AbstractController {
 	
 	@Autowired
 	private FeePaymentService feePaymentService;
+	
+	@Autowired
+	private RouteOfferService routeOfferService;
 	
 	static Logger log = Logger.getLogger(PayPalUserController.class);
 
@@ -97,6 +101,7 @@ public class PayPalUserController extends AbstractController {
 					so = shipmentOfferService.accept(so.getId());
 				result = new ModelAndView("redirect:/shipmentOffer/user/list.do?shipmentId=" + so.getShipment().getId());
 			}else{
+				routeOfferService.save(routeOfferService.findOne(po.getFeePayment().getRouteOffer().getId()));
 				result = new ModelAndView("redirect:/routeOffer/user/list.do?routeId=" + po.getFeePayment().getRouteOffer().getRoute().getId());
 			}
 			// Pagado y registrado correctamente
