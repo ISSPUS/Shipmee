98458a9715b533f49f98dbe39ed5843381cbcbeb
barmardom <bmardom@gmail.com>
2017-07-08 19:55:13 +0200

    Añadido safeHTML a domain de alert y routeForm

--- src/main/java/domain/Alert.java
+++ src/main/java/domain/Alert.java
@@ -12,6 +12,8 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.SafeHtml;
+import org.hibernate.validator.constraints.SafeHtml.WhiteListType;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
@@ -36,6 +38,7 @@ public class Alert extends DomainEntity {
 	
 	@NotNull
 	@NotBlank
+	@SafeHtml(whitelistType = WhiteListType.NONE)
 	public String getOrigin() {
 		return origin;
 	}
@@ -45,6 +48,7 @@ public class Alert extends DomainEntity {
 	
 	@NotNull
 	@NotBlank
+	@SafeHtml(whitelistType = WhiteListType.NONE)
 	public String getDestination() {
 		return destination;
 	}
